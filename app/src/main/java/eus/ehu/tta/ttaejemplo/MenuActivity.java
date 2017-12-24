b1e595e9d157962537305839743328aa67f4a4ab
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-12-24 13:27:33 +0100

    Añadidas nuevas actividades para realizar un nuevo test y un nuevo ejercicio. Los botones correspondientes en la actividad menuActivity permiten acceder a estas actividades

--- app/src/main/java/eus/ehu/tta/ttaejemplo/MenuActivity.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/MenuActivity.java
@@ -1,15 +1,30 @@
 package eus.ehu.tta.ttaejemplo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
     public static final String EXTRA_LOGIN = "login";
-
+    private Business business;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        this.business = new Business();
+    }
+
+    public void newTest(View view)
+    {
+        Intent intent = new Intent(this,newTestActivity.class);
+        startActivity(intent);
+    }
+
+    public void newExercise(View view)
+    {
+        Intent intent = new Intent(this,newExerciseActivity.class);
+        startActivity(intent);
     }
 }
