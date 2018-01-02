057451784ff3b0e7af7d7279d93a220b39b0687e
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-02 15:41:27 +0100

    Realizados cambios para que se guarde el estado de la actividad de nuevo test

--- app/src/main/java/eus/ehu/tta/ttaejemplo/TestTTA.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/TestTTA.java
@@ -1,9 +1,11 @@
 package eus.ehu.tta.ttaejemplo;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class TestTTA
-{
+public class TestTTA implements Parcelable {
     private int idEjercicio;
     private int correcta;
     private String pregunta;
@@ -49,4 +51,41 @@ public class TestTTA
     public void setAyuda(String ayuda) {
         this.ayuda = ayuda;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.idEjercicio);
+        dest.writeInt(this.correcta);
+        dest.writeString(this.pregunta);
+        dest.writeStringList(this.opciones);
+        dest.writeString(this.ayuda);
+    }
+
+    public TestTTA() {
+    }
+
+    protected TestTTA(Parcel in) {
+        this.idEjercicio = in.readInt();
+        this.correcta = in.readInt();
+        this.pregunta = in.readString();
+        this.opciones = in.createStringArrayList();
+        this.ayuda = in.readString();
+    }
+
+    public static final Parcelable.Creator<TestTTA> CREATOR = new Parcelable.Creator<TestTTA>() {
+        @Override
+        public TestTTA createFromParcel(Parcel source) {
+            return new TestTTA(source);
+        }
+
+        @Override
+        public TestTTA[] newArray(int size) {
+            return new TestTTA[size];
+        }
+    };
 }
--- app/src/main/java/eus/ehu/tta/ttaejemplo/newTestActivity.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/newTestActivity.java
@@ -14,9 +14,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class newTestActivity extends AppCompatActivity {
+    private static final java.lang.String STATE = "state";
+    private static final String CURRENT_TEST = "current_test";
+    private static final java.lang.String ANSWER_CHOSEN = "answer_chosen";
+    private static final java.lang.String IS_HELP_OPEN = "is_help_open";
+    private static final short TEST_START = 0;
+    private static final short RADIO_BUTTON_PRESSED = 1;
+    private static final short INCORRECT_ANSWER =2;
+    private static final short CORRECT_ANSWER = 3;
     private BusinessInterface business;
-    private boolean isRadioButtonSelected = false;
-    BusinessInterface.TestTTA currentTest;
+    private short state;
+    private TestTTA currentTest;
+    private int answerChosen = -1;
+    private boolean isHelpOpen=false;
     private static final String TAG = newTestActivity.class.getName();
 
     @Override
@@ -24,65 +34,89 @@ public class newTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_test);
         business = new Business();
+        if (savedInstanceState != null)
+        {
+            state = savedInstanceState.getShort(STATE);
+            currentTest = savedInstanceState.getParcelable(CURRENT_TEST);
+            answerChosen = savedInstanceState.getInt(ANSWER_CHOSEN);
+            isHelpOpen = savedInstanceState.getBoolean(IS_HELP_OPEN);
+        }
+
+        else
+        {
+            state = TEST_START;
+            currentTest = business.getNewTest();
+        }
         createViews();
+    }
+
+    public void onSaveInstanceState (Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putShort(STATE,state);
+        savedInstanceState.putParcelable(CURRENT_TEST,currentTest);
+        savedInstanceState.putInt(ANSWER_CHOSEN,answerChosen);
+        savedInstanceState.putBoolean(IS_HELP_OPEN,isHelpOpen);
     }
 
     private void createViews()
     {
-        final LinearLayout linearLayout = findViewById(R.id.newTestLayout);
-        currentTest = business.getNewTest();
-        TextView questionView = new TextView(this);
-        questionView.setText(currentTest.getPregunta());
-        linearLayout.addView(questionView);
-        final RadioGroup radioGroup = new RadioGroup(this);
-        radioGroup.getChildAt(2);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (!isRadioButtonSelected)
-                {
-                    isRadioButtonSelected=true;
-                    final Button sendButton = new Button(newTestActivity.this);
-                    sendButton.setText(R.string.send);
-                    sendButton.setOnClickListener(new Button.OnClickListener()
-                    {
-                        @Override
-                        public void onClick(View view)
-                        {
-                            int radioButtonID = radioGroup.getCheckedRadioButtonId();
-                            View radioButton = radioGroup.findViewById(radioButtonID);
-                            int index = radioGroup.indexOfChild(radioButton);
-                            Log.d(TAG,"Radio button " + index + " selected");
-                            if (business.sendTest(currentTest.getIdEjercicio(),index))
-                            {
-                                Toast.makeText(newTestActivity.this,R.string.success, Toast.LENGTH_SHORT).show();
-                            }
+        LinearLayout linearLayout = findViewById(R.id.newTestLayout);
 
-                            else
-                            {
-                                radioButton.setBackgroundColor(getResources().getColor(R.color.red));
-                                Toast.makeText(newTestActivity.this,R.string.failure, Toast.LENGTH_SHORT).show();
-                                Button helpButton = new Button(newTestActivity.this);
-                                helpButton.setText(R.string.getHelp);
-                                helpButton.setOnClickListener(new helpOnClickListener());
-                                linearLayout.addView(helpButton);
-                            }
-                            View correctRadioButton = radioGroup.getChildAt(currentTest.getCorrecta());
-                            correctRadioButton.setBackgroundColor(getResources().getColor(R.color.green));
-                            linearLayout.removeView(sendButton);
-                        }
-                    });
-                    linearLayout.addView(sendButton);
-                }
-            }
-        });
+        TextView questionView = findViewById(R.id.newTestTextView);
+        questionView.setText(currentTest.getPregunta());
+        RadioGroup radioGroup = findViewById(R.id.newTestRadioGroup);
+        radioGroup.setOnCheckedChangeListener(new radioOnCheckedListener ());
         for (String opcion : currentTest.getOpciones())
         {
             RadioButton optionView = new RadioButton(this);
             optionView.setText(opcion);
             radioGroup.addView(optionView);
         }
-        linearLayout.addView(radioGroup);
+        firstTimeSetStateViews();
+    }
+
+    private void firstTimeSetStateViews ()
+    {
+        setStateViews();
+        if (answerChosen!=-1)
+        {
+            RadioGroup radioGroup = findViewById(R.id.newTestRadioGroup);
+            ((RadioButton) radioGroup.getChildAt(answerChosen)).setChecked(true);
+        }
+        if (isHelpOpen)
+        {
+            openHelp();
+        }
+    }
+
+    private void setStateViews() {
+        if (state == RADIO_BUTTON_PRESSED)
+        {
+            LinearLayout linearLayout = findViewById(R.id.newTestLayout);
+            Button sendButton = new Button(newTestActivity.this);
+            sendButton.setText(R.string.send);
+            sendButton.setOnClickListener(new sendButtonOnClickListener());
+            sendButton.setId(R.id.newTestSendButton);
+            linearLayout.addView(sendButton);
+        }
+        else if (state == INCORRECT_ANSWER)
+        {
+            LinearLayout linearLayout = findViewById(R.id.newTestLayout);
+            RadioGroup radioGroup = findViewById(R.id.newTestRadioGroup);
+            View radioButton = radioGroup.getChildAt(answerChosen);
+            radioButton.setBackgroundColor(getResources().getColor(R.color.red));
+            Button helpButton = new Button(newTestActivity.this);
+            helpButton.setText(R.string.getHelp);
+            helpButton.setOnClickListener(new helpOnClickListener());
+            helpButton.setId(R.id.newTestHelpButton);
+            linearLayout.addView(helpButton);
+        }
+        if (state == CORRECT_ANSWER || state == INCORRECT_ANSWER)
+        {
+            RadioGroup radioGroup = findViewById(R.id.newTestRadioGroup);
+            View correctRadioButton = radioGroup.getChildAt(currentTest.getCorrecta());
+            correctRadioButton.setBackgroundColor(getResources().getColor(R.color.green));
+        }
     }
 
     private class helpOnClickListener implements View.OnClickListener
@@ -90,11 +124,65 @@ public class newTestActivity extends AppCompatActivity {
         @Override
         public void onClick (View view)
         {
+            if (!isHelpOpen)
+            {
+                openHelp();
+            }
+            else
+            {
+                closeHelp();
+            }
+            isHelpOpen = !isHelpOpen;
+        }
+    }
+
+    private void openHelp() {
+        TextView helpTextView = new TextView(newTestActivity.this);
+        helpTextView.setText(Html.fromHtml(currentTest.getAyuda()));
+        helpTextView.setTextColor(Color.BLACK);
+        helpTextView.setId(R.id.newTestHelpText);
+        LinearLayout linearLayout = findViewById(R.id.newTestLayout);
+        linearLayout.addView(helpTextView);
+    }
+
+    private void closeHelp() {
+        LinearLayout linearLayout = findViewById(R.id.newTestLayout);
+        linearLayout.removeView(findViewById(R.id.newTestHelpText));
+    }
+
+    private class radioOnCheckedListener implements RadioGroup.OnCheckedChangeListener
+    {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            answerChosen = group.indexOfChild(findViewById(checkedId));
+            if (state == TEST_START)
+            {
+                state=RADIO_BUTTON_PRESSED;
+                setStateViews();
+            }
+        }
+    }
+
+    private class sendButtonOnClickListener implements Button.OnClickListener
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (business.sendTest(currentTest.getIdEjercicio(),answerChosen))
+            {
+                state = CORRECT_ANSWER;
+                Toast.makeText(newTestActivity.this,R.string.success, Toast.LENGTH_SHORT).show();
+            }
+
+            else
+            {
+                state = INCORRECT_ANSWER;
+                Toast.makeText(newTestActivity.this,R.string.failure, Toast.LENGTH_SHORT).show();
+            }
             LinearLayout linearLayout = findViewById(R.id.newTestLayout);
-            TextView helpTextView = new TextView(newTestActivity.this);
-            helpTextView.setText(Html.fromHtml(currentTest.getAyuda()));
-            helpTextView.setTextColor(Color.BLACK);
-            linearLayout.addView(helpTextView);
+            View sendButton = findViewById(R.id.newTestSendButton);
+            linearLayout.removeView(sendButton);
+            setStateViews();
         }
     }
 }
