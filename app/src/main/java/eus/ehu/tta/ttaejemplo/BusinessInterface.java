f465f0b4eb20426b37678b7641213ffb832d5b6c
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-02 12:47:52 +0100

    Movida clase TestTTA a un fichero aparte

--- app/src/main/java/eus/ehu/tta/ttaejemplo/BusinessInterface.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/BusinessInterface.java
@@ -1,61 +1,10 @@
 package eus.ehu.tta.ttaejemplo;
 
-import java.util.List;
-
 /**
  * Created by jon on 12/22/17.
  */
 
 public interface BusinessInterface {
-
-    public class TestTTA
-    {
-        private int idEjercicio;
-        private int correcta;
-        private String pregunta;
-        private List<String> opciones;
-        private String ayuda;
-
-        public int getCorrecta() {
-            return correcta;
-        }
-
-        public void setCorrecta(int correcta) {
-            this.correcta = correcta;
-        }
-
-        public String getPregunta() {
-            return pregunta;
-        }
-
-        public void setPregunta(String pregunta) {
-            this.pregunta = pregunta;
-        }
-
-        public List<String> getOpciones() {
-            return opciones;
-        }
-
-        public void setOpciones(List<String> opciones) {
-            this.opciones = opciones;
-        }
-
-        public int getIdEjercicio() {
-            return idEjercicio;
-        }
-
-        public void setIdEjercicio(int idEjercicio) {
-            this.idEjercicio = idEjercicio;
-        }
-
-        public String getAyuda() {
-            return ayuda;
-        }
-
-        public void setAyuda(String ayuda) {
-            this.ayuda = ayuda;
-        }
-    }
     public abstract boolean login (String userName, String Password);
     public abstract TestTTA getNewTest();
     public abstract String getNewExercise();
---
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/TestTTA.java
@@ -0,0 +1,52 @@
+package eus.ehu.tta.ttaejemplo;
+
+import java.util.List;
+
+public class TestTTA
+{
+    private int idEjercicio;
+    private int correcta;
+    private String pregunta;
+    private List<String> opciones;
+    private String ayuda;
+
+    public int getCorrecta() {
+        return correcta;
+    }
+
+    public void setCorrecta(int correcta) {
+        this.correcta = correcta;
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public List<String> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(List<String> opciones) {
+        this.opciones = opciones;
+    }
+
+    public int getIdEjercicio() {
+        return idEjercicio;
+    }
+
+    public void setIdEjercicio(int idEjercicio) {
+        this.idEjercicio = idEjercicio;
+    }
+
+    public String getAyuda() {
+        return ayuda;
+    }
+
+    public void setAyuda(String ayuda) {
+        this.ayuda = ayuda;
+    }
+}
