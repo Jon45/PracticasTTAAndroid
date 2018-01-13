54b04b9f55c6e2aeec841af32b3d0d7b26331282
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-13 20:09:42 +0100

    Añadidos métodos para poder guardar objetos de la clase Opción en onSavedInstanceState

--- app/src/main/java/eus/ehu/tta/ttaejemplo/Opcion.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/Opcion.java
@@ -1,6 +1,9 @@
 package eus.ehu.tta.ttaejemplo;
 
-public class Opcion {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Opcion implements Parcelable {
     private String texto;
 
     public String getTexto() {
@@ -29,4 +32,37 @@ public class Opcion {
 
     private String ayuda;
     private String mimeTypeAyuda;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.texto);
+        dest.writeString(this.ayuda);
+        dest.writeString(this.mimeTypeAyuda);
+    }
+
+    public Opcion() {
+    }
+
+    protected Opcion(Parcel in) {
+        this.texto = in.readString();
+        this.ayuda = in.readString();
+        this.mimeTypeAyuda = in.readString();
+    }
+
+    public static final Parcelable.Creator<Opcion> CREATOR = new Parcelable.Creator<Opcion>() {
+        @Override
+        public Opcion createFromParcel(Parcel source) {
+            return new Opcion(source);
+        }
+
+        @Override
+        public Opcion[] newArray(int size) {
+            return new Opcion[size];
+        }
+    };
 }
