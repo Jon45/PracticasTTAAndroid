1a46dca2a4ccf1c85dfc5701c30d77f7edb89ea1
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2018-01-18 00:12:28 +0100

    Guardado nombre del usuario introducido para restaurar el editText de login

--- app/src/main/java/eus/ehu/tta/ttaejemplo/LoginActivity.java
+++ app/src/main/java/eus/ehu/tta/ttaejemplo/LoginActivity.java
@@ -1,7 +1,10 @@
 package eus.ehu.tta.ttaejemplo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +14,16 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String LOGINPREF = "login";
     private BusinessInterface business;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         this.business = new BusinessReal(getResources().getString(R.string.baseURL));
+        String login = getLogin();
+        EditText editText = findViewById(R.id.loginEditText);
+        editText.setText(login);
     }
 
     public void login(View view) {
@@ -27,6 +34,26 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
         password=passwordEditText.getText().toString();
         new LoginTask().execute(login,password);
+    }
+
+    public void saveLogin(String login)
+    {
+        SharedPreferences prefs = this.getSharedPreferences("eus.ehu.tta.ttaejemplo", Context.MODE_PRIVATE);
+        prefs.edit().putString(LOGINPREF,login).apply();
+    }
+
+    public String getLogin()
+    {
+        SharedPreferences prefs = this.getSharedPreferences("eus.ehu.tta.ttaejemplo", Context.MODE_PRIVATE);
+        return prefs.getString(LOGINPREF,"");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        EditText editText = findViewById(R.id.loginEditText);
+        saveLogin(editText.getText().toString());
     }
 
     private class LoginTask extends AsyncTask<String,Void,Boolean>
@@ -48,6 +75,9 @@ public class LoginActivity extends AppCompatActivity {
             super.onPostExecute(aBoolean);
             if (aBoolean.booleanValue())
             {
+                EditText editText = findViewById(R.id.loginEditText);
+                saveLogin(editText.getText().toString());
+
                 Intent intent = new Intent(LoginActivity.this,MenuActivity.class);
                 intent.putExtra(MenuActivity.EXTRA_LOGIN,UserData.getInstance().getUserName());
                 startActivity(intent);
